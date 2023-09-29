a51a2b78996501af35d8a7054adacf648ef066a9
amanknolus <aman.srivastava@knoldus.com>
2023-09-29 15:58:09 +0530

    triggering pipeline

--- pipeline.py
+++ pipeline.py
@@ -24,7 +24,8 @@ def pipeline(
 ):
     process_data_task = process_data().set_display_name("Process Data")
 
-    """Fit model changes trigger"""
+    """Making changes to trigger pipeline"""
+    """Fit model pipeline task"""
     fit_model_task = fit_model(BATCH_SIZE, process_data_task.output) \
         .after(process_data_task) \
         .set_display_name("Fit DB_Scan Model") \
